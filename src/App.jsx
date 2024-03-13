1abcb240a9f2939c03e18196b9548e3883f0bdff
pepe <pepe@gmail.com>
2024-03-13 13:18:16 -0500

    01

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+
+import Navegacion from "./components/Navegacion";
+
+function App() {
+
+  return (
+   <Navegacion></Navegacion>
+  )
+}
+
+export default App
---
+++ src/components/Navegacion.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Navegacion() {
+  return (
+    <>
+    <header className='h-screen w-full bg-gradient-to-r from-indigo-900 to-zinc-900'>
+      <nav className='w-full h-20 bg-gradient-to-r from-indigo-600 to-zinc-600 shadow-2xl'>
+        <div className='w-full h-full flex justify-center items-center'>
+          <img className='h-3/4 animate-shake animate-infinite animate-duration-[4000ms] animate-delay-1000 animate-ease-in-out' src="https://static.vecteezy.com/system/resources/thumbnails/013/758/832/small/abstract-letter-m-logo-illustration-in-trendy-and-minimal-style-png.png" alt="" srcset="" />
+        </div>
+      </nav>
+    </header>
+    
+    </>
+  )
+}
+
+export default Navegacion
